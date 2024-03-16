2510b94ca67e1177948b1cdf05d1e653708d8c49
Baptiste Courtin <bap2910@gmail.com>
2024-03-16 13:17:02 +0100

    ok

--- frontend/src/components/categories/SupprimerCategorie.tsx
+++ frontend/src/components/categories/SupprimerCategorie.tsx
@@ -1,13 +1,29 @@
 import axiosInstance from "@/lib/axiosInstance";
+import { useDeleteCategoryMutation } from "@/types/graphql";
 import router from "next/router";
 import React from "react";
 
 const SupprimerCategorie = ({ id }: any) => {
-  const deleteThisCategorie = () => {};
+  const [deleteCategory, { loading: loadingDelete, error: errorDelete }] =
+    useDeleteCategoryMutation();
+
+  function handleDelete(e: React.MouseEvent<HTMLButtonElement>): void {
+    if (id) {
+      deleteCategory({
+        variables: { deleteCategoryId: id },
+        onCompleted() {
+          router.push(`/categories/list`);
+        },
+        onError(error) {
+          console.error(error);
+        },
+      });
+    }
+  }
 
   return (
     <div className="adminDeleteCategorie">
-      <button onClick={deleteThisCategorie}>
+      <button onClick={handleDelete}>
         Supprimer cette categorie (cela supprime aussi les annonces)
       </button>
     </div>
--- frontend/src/pages/categories/[id].tsx
+++ frontend/src/pages/categories/[id].tsx
@@ -1,166 +1,80 @@
-// import CardAd from "@/components/ads/CardAd";
-// import SupprimerCategorie from "@/components/categories/SupprimerCategorie";
-
-// import Ad from "@/types/Ad";
-// import Category from "@/types/Category";
-
-// import axiosInstance from "@/lib/axiosInstance";
-// import axios, { CancelTokenSource } from "axios";
-
-// import Link from "next/link";
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/router";
-
-// import { useQuery } from "@apollo/client";
-// import { GET_CATEGORY_BY_ID } from "@/requetes/queries/categories.queries";
-// import {
-//   AdEntity,
-//   GetAdByIdQuery,
-//   GetCategoryByIdQuery,
-//   useGetCategoryByIdLazyQuery,
-//   useGetCategoryByIdQuery,
-// } from "@/types/graphql";
-
-// const CategoryId = () => {
-//   const router = useRouter();
-//   const { id } = router.query;
-
-//   // ------------------------- EXPRESS
-
-//   // const [category, setCategory] = useState<Category>();
-//   // const [messageError, setMessageError] = useState<Error>();
-
-//   // const getCategories = (source: CancelTokenSource) => {
-//   //   axiosInstance
-//   //     .get(`categories/find/${id}`, {
-//   //       cancelToken: source.token,
-//   //     })
-//   //     .then((response) => {
-//   //       setCategory(response.data);
-//   //       if (response.data.name == undefined) {
-//   //         setMessageError(response.data);
-//   //       }
-//   //     })
-//   //     .catch((err) => {
-//   //       if (err.code === "ERR_CANCELED") {
-//   //         console.warn("cancel request");
-//   //       } else {
-//   //         console.error(err);
-//   //       }
-//   //     });
-//   // };
-
-//   // useEffect(() => {
-//   //   if (router.isReady) {
-//   //     const source = axios.CancelToken.source();
-//   //     getCategories(source);
-//   //     return () => {
-//   //       source.cancel();
-//   //     };
-//   //   }
-//   // }, [router.isReady]);
-
-//   // ------------------------- GRAPHQL
-
-//   const [getCategoryById, { data, loading, error }] =
-//     useGetCategoryByIdLazyQuery();
-
-//   useEffect(() => {
-//     if (router.isReady) {
-//       getCategoryById({
-//         variables: { id: id as string },
-//         onError(err) {
-//           console.log("error", err);
-//         },
-//       });
-//     }
-//   }, [router.isReady]);
-
-//   return (
-//     <div className="listAdByCategoryId">
-//       {error ? (
-//         <h2>Une erreur... (déso)</h2>
-//       ) : loading ? (
-//         <h2>Chargement en cours</h2>
-//       ) : (
-//         <>
-//           <Link href={"/categories/list"} className="retourTopButton">
-//             ← Retour à la liste
-//           </Link>
-
-//           <SupprimerCategorie id={data?.getCategoryById.id} />
-
-//           <main>
-//             <h1>{data?.getCategoryById.name}</h1>
-
-//             <ul className="cardsAdUl">
-//               {data?.getCategoryById.ads.map(
-//                 (ad: GetCategoryByIdQuery["getCategoryById"]["ads"][0]) => (
-//                   <CardAd ad={ad} key={ad.id} /> // osef
-//                 )
-//               )}
-//             </ul>
-//           </main>
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CategoryId;
-
-import AdsGrid from "@/components/ads/AdsGrid";
+import CardAd from "@/components/ads/CardAd";
+import SupprimerCategorie from "@/components/categories/SupprimerCategorie";
 import Pagination from "@/components/ads/Pagination";
-import Back from "@/components/common/Back";
-import { useFindCategoryLazyQuery } from "@/types/graphql";
-import { useFindCategoryLazyQuery } from "@/types/graphql";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
-function CategoryId() {
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import {
+  GetCategoryByIdQuery,
+  useGetCategoryByIdLazyQuery,
+} from "@/types/graphql";
+
+const CategoryId = () => {
   const router = useRouter();
-  const [findCategory, { data, loading }] = useFindCategoryLazyQuery({
-    fetchPolicy: "no-cache",
-  });
+  const { id } = router.query;
+
+  const [getCategoryById, { data, loading, error }] =
+    useGetCategoryByIdLazyQuery();
 
   useEffect(() => {
     if (router.isReady) {
-      findCategory({
-        variables: { findCategoryId: router.query.id as string },
+      getCategoryById({
+        variables: { getCategoryByIdId: id as string },
+        onError(err) {
+          console.log("error", err);
+        },
       });
     }
   }, [router.isReady]);
 
-  const callRequest = (p: number, limit: number) => {
-    console.log(p, limit);
-    findCategory({
-      variables: {
-        findCategoryId: router.query.id as string,
-        limit,
-        skip: (p - 1) * limit,
-      },
-    });
-  };
+  //   const callRequest = (p: number, limit: number) => {
+  //     console.log(p, limit);
+  //     findCategory({
+  //       variables: {
+  //         findCategoryId: router.query.id as string,
+  //         limit,
+  //         skip: (p - 1) * limit,
+  //       },
+  //     });
+  //   };
 
-  if (loading) {
-    return <div>Chargement en cours</div>;
-  }
   return (
-    <div>
-      <Back />
-      <h1>{data?.findCategory.category.name}</h1>
-      {data?.findCategory.ads.length ? (
-        <>
-          <AdsGrid ads={data?.findCategory.ads} />
-          <Pagination
-            count={data?.findCategory.count}
-            callRequest={callRequest}
-          />
-        </>
+    <div className="listAdByCategoryId">
+      {error ? (
+        <h2>Une erreur... (déso)</h2>
+      ) : loading ? (
+        <h2>Chargement en cours</h2>
       ) : (
-        <div>Aucune annonce dans cette catégorie</div>
+        <>
+          <Link href={"/categories/list"} className="retourTopButton">
+            ← Retour à la liste
+          </Link>
+
+          <SupprimerCategorie id={data?.getCategoryById.category.id} />
+
+          <main>
+            <h1>{data?.getCategoryById.category.name}</h1>
+
+            <ul className="cardsAdUl">
+              {data?.getCategoryById.ads.map(
+                (ad: GetCategoryByIdQuery["getCategoryById"]["ads"][0]) => (
+                  <CardAd ad={ad} key={ad.id} /> // il manque category dans ad mais osef (ça marche pas quand j'essaie de la prendre)
+                )
+              )}
+            </ul>
+
+            {/* <AdsGrid ads={data?.findCategory.ads} />
+            <Pagination
+              count={data?.findCategory.count}
+              callRequest={callRequest}
+            /> */}
+          </main>
+        </>
       )}
     </div>
   );
-}
+};
+
 export default CategoryId;
